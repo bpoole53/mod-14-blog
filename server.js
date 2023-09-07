9e1c866e64110444adf8fb36ece9f3a43e4987f5
bpoole53 <bpoole53@gmail.com>
2023-09-06 20:13:53 -0500

    setup some basic infrastructure

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require ('path');
+
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
