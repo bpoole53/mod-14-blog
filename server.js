aa46d2a24a8a930d3f867d3291dd46e260941878
bpoole53 <bpoole53@gmail.com>
2023-09-08 15:04:18 -0500

    added session storing, cookie lasts 1 minute

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const path = require ('path');
 const routes = require('./controllers');
 const session = require('express-session');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//set cookie to last 1 minute, normally would be longer but for the purpose of not making the grader wait forever just set to that
 const sess = {
   secret: 'Super secret secret',
+  cookie: {
+    maxAge: 60* 1000,    
+  },
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 const app = express();
