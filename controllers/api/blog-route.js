e90ef53cc52a38c6ba9032ebaf7a026f798235e4
bpoole53 <bpoole53@gmail.com>
2023-09-07 21:51:33 -0500

    got blog posts to show up on the home page

--- controllers/api/blog-route.js
+++ controllers/api/blog-route.js
@@ -12,7 +12,10 @@ router.get('/', async (req, res) => {
   // route to create/add a blog post using async/await
 router.post('/', async (req, res) => {
   try { 
-    const blogData = await Blog.create(req.body);
+    const blogData = await Blog.create({
+        title: req.body.title,
+        description: req.body.description,
+    });
   // if the blog post is successfully created, the new response will be returned as json
   res.status(200).json(blogData)
 } catch (err) {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const Blog = require('../models/Blog')
 
 router.get('/', async (req, res) => {
-    res.render('home');
-});
+    const blogData = await Blog.findAll().catch((err) => { 
+        res.json(err);
+    });
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    res.render('home', { blogs });
+    });
 
 router.get('/blog', async (req, res) => {
     res.render('blog');
